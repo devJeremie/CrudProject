a87fac00e504e63454e4428c22b5b43e0ddc5d2f
devJeremie <chabanaisjeremie@gmail.com>
2024-02-10 12:00:40 +0100

    CrudProjectForCda/Dwwm

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Crud;
+use App\Form\CrudType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/create", name="app_main")
+     */
+    public function create(Request $request): Response
+    {
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+
+        return $this->render('main/createForm.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
