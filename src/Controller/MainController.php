9f8cb20dfda5990b94c8278d2cb9c59155eebefe
devJeremie <chabanaisjeremie@gmail.com>
2024-02-11 10:49:07 +0100

    EndCrudForStudents

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,16 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="app_main")
+     * @Route("/", name="main")
      */
     public function index(): Response
     {
+        $data = $this->getDoctrine()->getRepository(Crud::class)->findAll();
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'datas'=>$data,
         ]);
     }
     /**
-     * @Route("/create", name="app_main")
+     * @Route("/create", name="create")
      */
     public function create(Request $request): Response
     {
@@ -35,6 +37,8 @@ class MainController extends AbstractController
             $sendDatabase->flush();
 
             $this->addFlash('notice', 'Soumission réussi !!');
+
+            return $this->redirectToRoute('main');
         }
 
 
@@ -43,4 +47,48 @@ class MainController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+     /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, $id): Response
+    {
+
+        $crud = $this->getDoctrine()->getRepository(Crud::class)->find($id);
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+        if ( $form->isSubmitted() && $form->isValid()) {
+            $sendDatabase = $this->getDoctrine()
+                                 ->getManager();
+            $sendDatabase->persist($crud);
+            $sendDatabase->flush();
+
+            $this->addFlash('notice', 'Modification réussi !!');
+
+            return $this->redirectToRoute('main');
+        }
+
+
+        return $this->render('main/updateForm.html.twig', [
+            'controller_name' => 'MainController',
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete($id): Response
+    {
+
+        $crud = $this->getDoctrine()->getRepository(Crud::class)->find($id);
+        $sendDatabase = $this->getDoctrine()
+                             ->getManager();
+        $sendDatabase->remove($crud);
+        $sendDatabase->flush();
+
+            $this->addFlash('notice', 'Suppression réussi !!');
+
+            return $this->redirectToRoute('main');
+        }
 }
